e3ad8fa25852e6a621c2b3bdf7d2dd3a3c97a4a8
Thomas Mailund <mailund@birc.au.dk>
2014-07-07 15:58:08 +0200

    summary of chains

--- R/mcmc.R
+++ R/mcmc.R
@@ -17,7 +17,7 @@
 #' @return The samples containing only the parameter columns.
 #' @export
 parameter_samples <- function(samples) {
-  samples[,!(names(samples) %in% c("log.prior","log.likelihood","log.posterior"))]
+  samples[,!(names(samples) %in% c("sample", "log.prior","log.likelihood","log.posterior"))]
 }
 
 #' @title Creates an object for manipulating CoalHMM MCMC chains
@@ -38,5 +38,33 @@ plot.coalhmm_chain <- function(chain, ...) {
   melted <- melt(parameters, id='sample')
   ggplot(melted, aes(x=sample, y=value)) + geom_point() + 
     facet_grid(variable~., scales = "free_y") + geom_smooth(method="loess") +
-    geom_vline(xintercept=convergence_point, color='red')
+    geom_vline(xintercept=convergence_point, color='red') + theme_bw()
+}
+
+summary.coalhmm_chain <- function(chain, ...) {
+  convergence_diag <- convergence_diagnostics(chain, ...)
+  convergence_point <- convergence_diag$convergence_point
+  parameters <- parameter_samples(chain[convergence_point:nrow(chain), ])
+
+  estimates <- apply(parameters, 2, quantile, probs=c(0.025, 0.25, 0.5, 0.75, 0.975))
+  
+  result <- list(parameters = parameters, estimates = estimates,
+                 convergence_point = convergence_point)
+  class(result) <- "coalhmm_chain_summary"
+  return(result)
+}
+
+print.coalhmm_chain_summary <- function(chain_summary) {
+  cat("Estimated convergence point is", chain_summary$convergence_point)
+  cat(" leaving", nrow(chain_summary$parameters), "samples\n\n")
+  
+  parameter_names <- colnames(chain_summary$estimates)
+  cat("Estimates (quantiles):\n")
+  print(chain_summary$estimates)
+  cat("\n")
+}
+
+plot.coalhmm_chain_summary <- function(chain_summary) {
+  ggplot(data=melt(chain_summary$parameters)) + geom_histogram(aes(x=value)) +
+    facet_grid(. ~ variable, scale="free") + theme_bw()
 }
